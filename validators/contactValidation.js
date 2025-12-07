73a1e1364db282af11d670a45766f2897ea6058d
Abhishek Sharma <developer.abhishek25@gmail.com>
2025-12-07 18:56:41 +0530

    Fix Railway start command

--- validators/contactValidation.js
+++ validators/contactValidation.js
@@ -1,7 +1,9 @@
 const { check } = require("express-validator");
 
-exports.validateContact = [
-  check("name", "Name is required").not().isEmpty(),
-  check("email", "Valid email is required").isEmail(),
-  check("message", "Message cannot be empty").not().isEmpty(),
+const validateContact = [
+  check("name").notEmpty().withMessage("Name is required"),
+  check("email").isEmail().withMessage("Valid email is required"),
+  check("message").notEmpty().withMessage("Message cannot be empty"),
 ];
+
+module.exports = { validateContact };
