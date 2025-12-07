d89329bec4c77fae3b303af3971ba20f20a413a0
Abhishek Sharma <developer.abhishek25@gmail.com>
2025-12-07 22:38:26 +0530

    clean install fix

--- config/dbConnections.js
+++ config/dbConnections.js
@@ -1,25 +1,45 @@
-const {DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME} = process.env;
-
-// var mysql = require('mysql');
 const mysql = require('mysql2');
 
-var conn = mysql.createConnection({
-     host: process.env.DB_HOST,
+const conn = mysql.createConnection({
+    host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT
 });
-// var conn = mysql.createConnection({
-//     host:DB_HOST,
-//     user:DB_USERNAME,
-//     password:DB_PASSWORD,
-//     database:DB_NAME
-// });
 
-conn.connect(function(err){
-    if(err) throw err;
-    // console.log(DB_NAME+'Database connected successfully!')
+conn.connect((err) => {
+    if (err) {
+        console.error("Database Connection Error:", err);
+        return;
+    }
+    console.log("Database connected successfully!");
 });
 
 module.exports = conn;
+
+// const {DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME} = process.env;
+
+// // var mysql = require('mysql');
+// const mysql = require('mysql2');
+
+// var conn = mysql.createConnection({
+//      host: process.env.DB_HOST,
+//     user: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT
+// });
+// // var conn = mysql.createConnection({
+// //     host:DB_HOST,
+// //     user:DB_USERNAME,
+// //     password:DB_PASSWORD,
+// //     database:DB_NAME
+// // });
+
+// conn.connect(function(err){
+//     if(err) throw err;
+//     // console.log(DB_NAME+'Database connected successfully!')
+// });
+
+// module.exports = conn;
